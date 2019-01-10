40f441b11e8fd3e6eb795bc9626ef271df367307
Mark Young <marky@lunarg.com>
2019-01-10 11:37:46 -0700

    Update copyright headers in files
    Updated the copyright headers to use the SPDX ID for the license
    to reduce the prefixing comment size.  Also cleaned up the rest
    of the comment to be much more concise.
    
    Finally, I noticed that the old LICENSE.txt file was still around.
    Removed it, updated the LICENSE file with the comment that other
    files in the repo contain non-Apache 2.0 licensed material and
    removed the COPYRIGHT.txt file.

--- globe/globe_clock.cpp
+++ globe/globe_clock.cpp
@@ -1,22 +1,11 @@
-/*
- * LunarGlobe - globe_clock.cpp
- *
- * Copyright (C) 2018 LunarG, Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- * Author: Mark Young <marky@lunarg.com>
- */
+//
+// Project:                 LunarGlobe
+// SPDX-License-Identifier: Apache-2.0
+//
+// File:                    globe/globe_clock.cpp
+// Copyright(C):            2018-2019; LunarG, Inc.
+// Author(s):               Mark Young <marky@lunarg.com>
+//
 
 #if defined(VK_USE_PLATFORM_XCB_KHR) || defined(VK_USE_PLATFORM_XLIB_KHR) || defined(VK_USE_PLATFORM_WAYLAND_KHR)
 #include "linux/globe_clock_linux.hpp"
